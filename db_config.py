a11c780bd14f9bd0034e72fd70133e1a1a2fc14f
Rahuls-iMac <rahwinside@gmail.com>
2021-04-22 09:58:55 +0530

    Update db info

--- db_config.py
+++ db_config.py
@@ -7,7 +7,7 @@ mysql = MySQL(cursorclass=DictCursor)
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = "tux"
-app.config['MYSQL_DATABASE_PASSWORD'] = "Licet@123"
+app.config['MYSQL_DATABASE_PASSWORD'] = "licet@123"
 app.config['MYSQL_DATABASE_DB'] = "miles"
-app.config['MYSQL_DATABASE_HOST'] = "vmlinuz.pattarai.in"
+app.config['MYSQL_DATABASE_HOST'] = "vpn.opencloud.pattarai.in"
 mysql.init_app(app)
